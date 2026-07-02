f4aadf7e5feb24ec3c208dabc5493dafdda90f3f
Anton Filistovich <anton.filistovich@gmail.com>
2026-07-03 01:42:26 +0300

    Инкремент 1. Сервис сокращения URL
    Реализован HTTP-сервер на Go, который:
    - принимает POST / с длинным URL (text/plain) и возвращает 201 с коротким URL
    - обрабатывает GET /{id} и делает 307 редирект на оригинальный URL
    - возвращает 400 на некорректные запросы
    - использует in-memory хранилище с sync.RWMutex

--- main.go
+++ main.go
@@ -1,20 +1,143 @@
 package main
 
 import (
+	"crypto/rand"
 	"fmt"
+	"io"
+	"log"
+	"math/big"
+	"net/http"
+	"net/url"
+	"strings"
+	"sync"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
-func main() {
-	//TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-	// to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-	s := "gopher"
-	fmt.Printf("Hello and welcome, %s!\n", s)
+const (
+	serverHost = "localhost:8080"
+	idLength   = 8
+	idAlphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+)
 
-	for i := 1; i <= 5; i++ {
-		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		fmt.Println("i =", 100/i)
+var (
+	urlStorage = map[string]string{}
+	storeMu    sync.RWMutex
+)
+
+func isValidUrl(s string) bool {
+	u, err := url.ParseRequestURI(s)
+	if err != nil {
+		return false
+	}
+
+	if u.Host == "" {
+		return false
+	}
+
+	return u.Scheme == "http" || u.Scheme == "https"
+}
+
+func generateID() (string, error) {
+	b := make([]byte, idLength)
+	n := big.NewInt(int64(len(idAlphabet)))
+	for i := range b {
+		idx, err := rand.Int(rand.Reader, n)
+		if err != nil {
+			return "", fmt.Errorf("failed to generate random data")
+		}
+		b[i] = idAlphabet[idx.Int64()]
+	}
+	return string(b), nil
+}
+
+func shortenHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "bad request", http.StatusMethodNotAllowed)
+		return
+	}
+
+	if r.Header.Get("Content-Type") != "text/plain" {
+		http.Error(w, "Content-Type must be text/plain", http.StatusBadRequest)
+		return
+	}
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+
+	rawUrl := strings.TrimSpace(string(body))
+
+	if rawUrl == "" || !isValidUrl(rawUrl) {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	var id string
+
+	for {
+		candidate, err := generateID()
+		if err != nil {
+			http.Error(w, "internal error", http.StatusInternalServerError)
+			return
+		}
+		storeMu.RLock()
+		_, exist := urlStorage[candidate]
+		storeMu.RUnlock()
+
+		if !exist {
+			id = candidate
+			break
+		}
+	}
+
+	storeMu.Lock()
+	urlStorage[id] = rawUrl
+	storeMu.Unlock()
+
+	shortURL := "http://" + serverHost + "/" + id
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusCreated)
+	_, _ = w.Write([]byte(shortURL))
+}
+
+func redirectHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "bad request", http.StatusMethodNotAllowed)
+		return
+	}
+
+	id := strings.TrimPrefix(r.URL.Path, "/")
+	if id == "" {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	storeMu.RLock()
+	original, exist := urlStorage[id]
+	storeMu.RUnlock()
+
+	if !exist {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	w.Header().Set("Location", original)
+	w.WriteHeader(http.StatusTemporaryRedirect)
+}
+
+func mainHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		shortenHandler(w, r)
+		return
+	}
+	redirectHandler(w, r)
+}
+
+func main() {
+	log.Printf("URL shortener is running on http://%s", serverHost)
+	if err := http.ListenAndServe(serverHost, http.HandlerFunc(mainHandler)); err != nil {
+		log.Fatal(err)
 	}
 }
