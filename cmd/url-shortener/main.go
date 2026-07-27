7e542a6e0168830f97d9980bed059004e4c00aa0
Anton Filistovich <anton.filistovich@gmail.com>
2026-07-27 17:50:47 +0300

    feat: add storage, config, logger, mwLogger

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,6 +1,71 @@
 package main
 
 import (
+	"URLShortener/internal/config"
+	"URLShortener/internal/http-server/middleware/mwLogger"
+	"URLShortener/internal/lib/logger/sl"
+	"URLShortener/internal/storage/sqlite"
+	"fmt"
+	"log/slog"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+	fmt.Println(cfg)
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting url-shortener", slog.String("environment", cfg.Env))
+	log.Debug("debug logging enabled")
+
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+
+	router := chi.NewRouter()
+	router.Use(middleware.RequestID)
+	router.Use(middleware.Logger)
+	router.Use(mwLogger.New(log))
+	router.Use(middleware.Recoverer)
+	router.Use(middleware.URLFormat)
+
+	// TODO init server:
+}
+
+func setupLogger(env string) *slog.Logger {
+	var logger *slog.Logger
+
+	switch env {
+	case envLocal:
+		logger = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		logger = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		logger = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return logger
+}
+
+/*
+import (
 	"crypto/rand"
 	"io"
 	"log"
@@ -140,3 +205,4 @@ func main() {
 		log.Fatal(err)
 	}
 }
+*/
--- go.mod
+++ go.mod
@@ -1,3 +1,24 @@
 module URLShortener
 
 go 1.26
+
+require github.com/ilyakaznacheev/cleanenv v1.5.0
+
+require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/go-chi/chi/v5 v5.3.1 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mattn/go-sqlite3 v1.14.48 // indirect
+	github.com/ncruces/go-strftime v1.0.0 // indirect
+	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
+	golang.org/x/sys v0.46.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	modernc.org/libc v1.74.1 // indirect
+	modernc.org/mathutil v1.7.1 // indirect
+	modernc.org/memory v1.11.0 // indirect
+	modernc.org/sqlite v1.54.0 // indirect
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
+)
